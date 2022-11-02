e47212467940b62878d24eed052b8a9c106d5b45
demme <demmerleraph@hotmail.com>
2022-11-03 00:30:09 +0100

    first commit

---
+++ src/api/user_api.rs
@@ -0,0 +1,31 @@
+use crate::{models::user_model::User, repository::mongodb_repo::MongoRepo};
+use mongodb::results::InsertOneResult;
+use rocket::{http::Status, serde::json::Json, State};
+
+#[post("/user", data = "<new_user>")]
+pub fn create_user(
+    db: &State<MongoRepo>,
+    new_user: Json<User>,
+) -> Result<Json<InsertOneResult>, Status> {
+    let data = User {
+        id: None,
+        firstName: new_user.firstName.to_owned(),
+        lastName: new_user.lastName.to_owned(),
+        email: new_user.email.to_owned(),
+        role: new_user.role.to_owned(),
+    };
+    let user_detail = db.create_user(data);
+    match user_detail {
+        Ok(user) => Ok(Json(user)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
+
+#[get("/users")]
+pub fn get_all_users(db: &State<MongoRepo>) -> Result<Json<Vec<User>>, Status> {
+    let users = db.get_all_users();
+    match users {
+        Ok(users) => Ok(Json(users)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
---
+++ src/repository/mongodb_repo.rs
@@ -0,0 +1,54 @@
+use std::env;
+extern crate dotenv;
+use dotenv::dotenv;
+
+use mongodb::{
+    bson::{extjson::de::Error},
+    results::{ InsertOneResult},
+    sync::{Client, Collection},
+};
+use crate::models::user_model::User;
+
+pub struct MongoRepo {
+    col: Collection<User>,
+}
+
+impl MongoRepo {
+    pub fn init() -> Self {
+        dotenv().ok();
+        let uri = match env::var("MONGOURI") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error loading env variable"),
+        };
+        let client = Client::with_uri_str(uri).unwrap();
+        let db = client.database("seriousgame");
+        let col: Collection<User> = db.collection("users");
+        MongoRepo { col }
+    }
+
+    pub fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+        let new_doc = User {
+            id: None,
+            firstName: new_user.firstName,
+            lastName: new_user.lastName,
+            email: new_user.email,
+            role: new_user.role,
+        };
+        let user = self
+            .col
+            .insert_one(new_doc, None)
+            .ok()
+            .expect("Error creating user");
+        Ok(user)
+    }
+
+    pub fn get_all_users(&self) -> Result<Vec<User>, Error> {
+        let cursors = self
+            .col
+            .find(None, None)
+            .ok()
+            .expect("Error getting list of users");
+        let users = cursors.map(|doc| doc.unwrap()).collect();
+        Ok(users)
+    }
+}
